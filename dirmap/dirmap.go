e442a20939b4f135f0a8a48f5a57013aab087960
Thura Hlaing <trhura@gmail.com>
2014-02-09 18:38:07 +0630

    added more func for DirMap

--- dirmap/dirmap.go
+++ dirmap/dirmap.go
@@ -1,10 +1,30 @@
 package dirmap
 
 type DirMap struct {
-	dirmap map[string] OrderedSet
+	dirmap map[string] *OrderedSet
 }
 
 func New() *DirMap {
 	d := new(DirMap)
 	return d
+}
+
+func (d *DirMap) Add(basename string, fullpath string) {
+	if _, exists := d.dirmap[basename]; exists == false {
+		d.dirmap[basename] = NewOrderedSet()
+	}
+
+	d.dirmap[basename].Push(fullpath)
+}
+
+func (d *DirMap) Has(basename string) bool {
+	_, exists := d.dirmap[basename]
+	return exists
+}
+
+func (d *DirMap) Get(basename string) (path string) {
+	if _, exists := d.dirmap[basename]; exists == true {
+		//return d.dirmap[basename].Get(0)
+	}
+	return path
 }
