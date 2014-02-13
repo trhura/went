b8e958dd5081a0bb2d0f34b139f17c2b13c3b044
Thura Hlaing <trhura@gmail.com>
2014-02-13 10:53:48 +0630

    added some more tests

--- tests.sh
+++ tests.sh
@@ -2,16 +2,27 @@
 
 source "./assert.sh"
 
-# Build binary
+# Build go binary
 go build went.go
+WENT=$(pwd)/went
 
-# use tmp directory for recentf file
+# use tmp directory to store recently visited db for testing
 HOME="/tmp"
 rm -f "$HOME/.went.recentf"
 
-assert "./went $PWD" "$PWD"
-assert "./went $(basename $PWD)" "$PWD"
+# It should
+assert "$WENT $PWD" "$PWD"
+assert "$WENT $(basename $PWD)" "$PWD"
 
-
+# Test going parent directories using `...`
+mkdir -p /tmp/testwent/4/3/2/1
+LEAFCHILD=/tmp/testwent/4/3/2/1
+builtin cd $LEAFCHILD
+assert "basename $($WENT ..)" "2"
+builtin cd $LEAFCHILD
+assert "basename $($WENT ...)" "3"
+builtin cd $LEAFCHILD
+assert "basename $($WENT ....)" "4"
+rm -rf /tmp/testwent
 
 assert_end
