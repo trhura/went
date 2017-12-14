2f592832d0e81189a64e2b537d0e27d34c5b1465
Thura Hlaing <trhura@gmail.com>
2017-12-14 11:23:16 +0630

    fixes a bug to remove defunct directories

--- went.py
+++ went.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 # Author: Thura Hlaing <trhura@gmail.com>
-# Time-stamp: <2017-11-17 10:41:04 (thurahlaing)>
+# Time-stamp: <2017-11-27 09:12:50 (thurahlaing)>
 
 __author__ = "Thura Hlaing <trhura@gmail.com>"
 __copyright__ = "Copyright 2017, Planet Earth"
@@ -10,18 +10,24 @@ import sys
 import pickle
 import os.path
 
+
 class RecentlyVisitedDirectories:
     pathsbydir = {}
     CONFIG_FILE = os.path.expandvars("$HOME/.went.directories")
-    
+
+    def get_recently_visited_directories (self):
+        return self.pathsbydir.keys()
+
     def get_recently_visited (self, dirname):
-        if not dirname in self.pathsbydir: return ""
-        
+        if dirname not in self.pathsbydir: return ""
+
         path = self.pathsbydir[dirname][0]
         if os.path.exists(path):
             return path
-        
-        del self.pathsbydir[dirname][0]
+
+        del self.pathsbydir[dirname][0]  # delete if the path no longer exists
+        # delete directory if no paths exist
+        if len(self.pathsbydir[dirname]) == 0: del self.pathsbydir[dirname] # noqa
         return self.get_recently_visited(dirname)
 
     def get_next_recently_visited (self, dirname):
@@ -30,7 +36,7 @@ class RecentlyVisitedDirectories:
         # shift the list by putting index 0 to the end
         self.pathsbydir[dirname] = self.pathsbydir[dirname][1:] + [self.pathsbydir[dirname][0]]
         return self.get_recently_visited(dirname)
-    
+
     def add_recently_visited (self, path):
         if not(os.path.exists(path) and os.path.isdir(path)): return
 
@@ -39,7 +45,7 @@ class RecentlyVisitedDirectories:
             self.pathsbydir[dirname] = [path]
         else:
             self.pathsbydir[dirname] = [path] + [p for p in self.pathsbydir[dirname] if p != path]
-    
+
     def __init__(self):
         try:
             with open(self.CONFIG_FILE, 'rb') as configFile:
@@ -50,12 +56,12 @@ class RecentlyVisitedDirectories:
         for k, v in self.pathsbydir.items():
             if not v: del self.pathsbydir[k] # if empty paths, remove
             self.pathsbydir[k] = v[:3] # only save the firs three paths
-            
+
         with open(self.CONFIG_FILE, 'wb') as configFile:
             pickle.dump (self.pathsbydir, configFile)
 
 WORKING_DIRECTORY = os.getcwd()
-RECENT_DIRECTORIES = RecentlyVisitedDirectories() 
+RECENT_DIRECTORIES = RecentlyVisitedDirectories()
 
 def go_home_if_nowhere(destination):
     """ Go to home directory if no argument was passed. """
@@ -77,7 +83,7 @@ def go_parents_if_dots(destination):
 def go_there_if_path (destination):
     """ Go to specified path for absolute and relative paths (aka contains slash). """
     if os.path.isabs(destination): return destination
-    
+
     abspath = os.path.abspath(destination)
     return abspath if os.path.exists(abspath) else ""
 
@@ -95,18 +101,21 @@ DIRECTIONS = [
 def go(path):
     RECENT_DIRECTORIES.add_recently_visited(path)
     print(path)
-    
+
 def determine_wheretogo(destination):
     for direction in DIRECTIONS:
         path = direction(destination)
         if path: go(path); break
     else:
         # Ok, we don't know where to go, but `cd` may know
-        go(destination) 
-        
+        go(destination)
+
 def main():
     destination = sys.argv[1] if len(sys.argv) > 1 else None
-    determine_wheretogo(destination)
-    RECENT_DIRECTORIES.__save__()
+    if destination == "__completions__":
+        print(" ".join(RECENT_DIRECTORIES.get_recently_visited_directories()))
+    else:
+        determine_wheretogo(destination)
+        RECENT_DIRECTORIES.__save__()
 
-if __name__ == "__main__": main() 
+if __name__ == "__main__": main()
